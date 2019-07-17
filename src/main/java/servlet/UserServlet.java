60f9a7aa8ebedd1d51ecfacb6f12a5d898a7be03
HHQ <299299297@qq.com>
2019-07-17 21:04:23 +0800

    eclie 01

--- src/main/java/servlet/UserServlet.java
+++ src/main/java/servlet/UserServlet.java
@@ -23,6 +23,7 @@ public class UserServlet extends HttpServlet {
 		writer.print("scuccess");
 		System.out.println("a2");
 		System.out.println("a1");
+		System.out.println("a2");
 		writer.close();
 	}
 
