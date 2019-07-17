b2ed9efff35893c4f7bea5112a734420bfda871b
HHQ <299299297@qq.com>
2019-07-17 19:55:54 +0800

    master init 01

---
+++ src/main/java/servlet/UserServlet.java
@@ -0,0 +1,27 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/user")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request,HttpServletResponse response) 
+	throws ServletException, IOException{
+		System.out.println("接收到请求");
+		PrintWriter writer = response.getWriter();
+		writer.print("scuccess");
+		writer.close();
+	}
+
+}
